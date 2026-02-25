b4faf2d0ed61d33d3565d3cdeaa2c80499a8cc69
riorajhon <riora19930303@outlook.com>
2026-02-25 10:11:47 -0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -122,10 +122,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
+        "http://151.241.216.178:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
